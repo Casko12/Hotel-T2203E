9569bbbc9909914a9ae063c2f71c928284120601
Hunghh90 <hungbkvn9x1@gmail.com>
2022-10-25 20:38:30 +0700

    Hotel

--- src/hotel/Main.java
+++ src/hotel/Main.java
@@ -7,9 +7,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    public static Stage rootStage;
     @Override
     public void start(Stage primaryStage) throws Exception{
-
+        rootStage = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
+        primaryStage.setTitle("Group 1 Hotel");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
     }
 
 
