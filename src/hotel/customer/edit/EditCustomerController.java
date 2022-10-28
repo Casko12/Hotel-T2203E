6852753dabf59a4886bea1723c9d500ff288f4e5
Casko12 <phamduyhung.ftu@gmail.com>
2022-10-28 19:09:50 +0700

    Hotel

--- src/hotel/customer/edit/EditCustomerController.java
+++ src/hotel/customer/edit/EditCustomerController.java
@@ -1,15 +1,21 @@
 package hotel.customer.edit;
 
 import hotel.Main;
+import hotel.dao.impls.CustomerRepository;
 import hotel.entities.Customer;
+import hotel.enums.RepoType;
+import hotel.factory.RepositoryFactory;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 
@@ -33,9 +39,39 @@ public class EditCustomerController implements Initializable {
     }
 
     public void submit(ActionEvent actionEvent) throws Exception {
-
+        try{
+            String name = txtName.getText();
+            String cmt = txtCMT.getText();
+            editedCustomer.setName(name);
+            editedCustomer.setCmt(cmt);
+            CustomerRepository rp = (CustomerRepository) RepositoryFactory.creHotelRepository(RepoType.CUSTOMER);
+            if(rp.update(editedCustomer)){
+                backToList(null);
+            }else {
+                System.out.println("Error!");
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 
     public void delete(ActionEvent actionEvent) throws Exception {
+        try{
+            Alert alert= new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Delete Customer?");
+            alert.setHeaderText("Are you sure delete customer: "+editedCustomer.getName());
+            Optional<ButtonType> option = alert.showAndWait();
+
+            if(option.get() == ButtonType.OK){
+                CustomerRepository rp = (CustomerRepository) RepositoryFactory.creHotelRepository(RepoType.CUSTOMER);
+                if(rp.delete(editedCustomer)){
+                    backToList(null);
+                }else {
+                    System.out.println("Error");
+                }
+            }
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
