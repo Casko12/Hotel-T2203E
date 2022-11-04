3637a59b2e302ee25474c8843083d8198fe8276b
Nguyenthai27 <ptejthaii@gmail.com>
2022-11-04 20:09:12 +0700

    upcode

--- src/hotel/Main.java
+++ src/hotel/Main.java
@@ -16,8 +16,6 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
-
-
     public static void main(String[] args) {
         launch(args);
     }
