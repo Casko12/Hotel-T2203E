4624d027f88884fd692e0cc821235bce60b71c89
Administrator <tranvanthanh060699@gmail.com>
2022-11-06 23:33:09 +0700

    up

--- src/hotel/roomrent/checkout/CheckOutController.java
+++ src/hotel/roomrent/checkout/CheckOutController.java
@@ -10,11 +10,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class CheckOutController implements Initializable {
@@ -40,12 +43,19 @@ public class CheckOutController implements Initializable {
 
     public void checkOut(ActionEvent actionEvent) {
         try {
-            RoomRentRepository rr = new RoomRentRepository();
-            ArrayList ls = new ArrayList<>();
-            ls.addAll(rr.all());
-            if (rr.checkin(checkout)){
-                bttBack(null);
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Check Out ");
+            alert.setHeaderText("You want to Checkout? "+checkout.getRoomName());
+            Optional<ButtonType> option = alert.showAndWait();
+            if (option.get()==ButtonType.OK){
+                RoomRentRepository rr = new RoomRentRepository();
+                ArrayList ls = new ArrayList<>();
+                ls.addAll(rr.all());
+                if (rr.checkin(checkout)){
+                    bttBack(null);
+                }
             }
+
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
